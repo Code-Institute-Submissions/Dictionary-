9b7434dbf4f1cd2758e4585b3c5a8852d25b534e
harrisonmorgana <harrison.morgana@outlook.com>
2020-10-27 22:31:58 +0000

    removed base HTML as no longer needed, using seperate HTML files instead.

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("/index.html")
 
 
 @app.route('/villians')
 def villians():
-    return render_template("villians.html")
+    return render_template("/villians.html")
 
 @app.route('/heroes')
 def heroes():
-    return render_template("heroes.html")
+    return render_template("/heroes.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
