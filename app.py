ade4e8780065145f9bfef59f358c0bb686bfa9ac
harrisonmorgana <harrison.morgana@outlook.com>
2020-11-02 12:33:30 +0000

    created flask app

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 import os
 from flask import Flask, render_template
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template("/index.html")
+def hello():
+    return "Hello World...Again!"
 
-
-@app.route('/villians')
-def villians():
-    return render_template("/villians.html")
-
-@app.route('/heroes')
-def heroes():
-    return render_template("/heroes.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
