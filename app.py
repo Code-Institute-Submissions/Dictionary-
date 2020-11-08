a5bf22c6e03e17b97a85fa149cfc9140ed379dcc
harrisonmorgana <harrison.morgana@outlook.com>
2020-11-08 22:46:50 +0000

    Fixed issues with information not being displayed in table format

--- app.py
+++ app.py
@@ -1,28 +1,45 @@
 import os
-from flask import Flask, (Render_tempate, redirect, request, session, url_For)
-from flask_pymongo import PyMongo
+from astropy.table import QTable, Table, Column
+from flask import Flask #(Render_tempate, redirect, request, session, url_For)
+from flask import render_template
+from flask import jsonify
+from flask_pymongo import pymongo
+from pymongo import MongoClient
 from bson.objectid import ObjectId
+
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secure_key = os.environ.get("SECRET_KEY")
-
-mongo = PyMongo(app)
-
+# Set Variables
+databaseConnection = os.environ.get("MONGO_URI")
+dbName = os.environ.get("MONGO_DBNAME")
 
 @app.route('/')
-@app.route("/get_heroes")
-def get_heroes():
-    heroes = mongo.db.heroes.find
-    return render_template("heroes.html", heroes=heroes)
+@app.route('/get-heroes/')
+def getHeroes():
+    # Connect Database
+    connection = MongoClient(databaseConnection)
+    database = connection.marvelHeroes
+    collection = database.Heroes
+    heroes = collection.find({})
+    allData = []
+    for hero in heroes:
+        name = str(hero.get("Name"))
+        alias = str(hero.get("Alias"))
+        #appearances = str(hero.get("First Comic Appearance"))
+        #cinematicAppearances = str(hero.get("Marvel Cinematic Appearance"))
+        #playedBy = str(hero.get("Played by"))
+        #createdBy = str(hero.get("Created by"))
+        data = name + "," + alias
+        allData.append(data)
+    headers = ["Name", "Alias"]
+    print(allData)
+    return render_template('index.html', headers=headers, values=allData)
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-
